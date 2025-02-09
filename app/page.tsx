c3115286af89565629e35d069d0c70b8cb06502e
Prodi <programer972@gmail.com>
2025-02-09 11:29:21 +0100

    connection   fonts

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='text-5xl font-bold'>page</div>
+    <div className='text-5xl'>page</div>
   )
 }
 
--- app/tour/tour-details/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TourDetailsDynamic = () => {
-  return (
-    <div>TourDetailsDynamic</div>
-  )
-}
-
-export default TourDetailsDynamic
