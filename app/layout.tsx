4e39b31d4ca8731bd2e65a8c9c2d6b8ae4867ca0
Prodi <programer972@gmail.com>
2025-02-14 13:08:38 +0100

    create  top button  with useEffect and window

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import {Poppins} from 'next/font/google'
 import "./globals.css";
 import ResponsiveNav from "@/components/Navbar/ResponsiveNav";
 import Footer from "@/components/Footer/Footer";
+import ScrollTopButton from "@/components/Helper/ScrollTopButton";
 
 const font = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -36,6 +37,7 @@ export default function RootLayout({
       <ResponsiveNav />
         {children}
       <Footer />
+      <ScrollTopButton />
       </body>
     </html>
   );
